9dd76c31c8d2f08a3a5d7fdd8c783d54d3161e52
ExplodingInsanity <32022907+ExplodingInsanity@users.noreply.github.com>
2017-12-12 22:02:32 +0200

    Reading from file
    Reading from our server rather than hardcoded values for the site

--- Server/Server.js
+++ Server/Server.js
@@ -1,61 +1,14 @@
 var http = require("http");
+var fs   = require("fs");
 
-var server = http.createServer(function(req, res){
-	res.writeHead(200,{"Content-Type": "text/html"});
-	res.end(`
-		<!DOCTYPE html>
-<html>
-<head>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<title>Main Page</title>
-</head>
-<body>
-	<div id="meniulateral"></div>
-	<div id="meniusus"></div>
-	<div id="restulpaginii"></div>
-</body>
-</html>
+var server = http.createServer(function(req,res){
+	res.writeHead(200, {"Content-Type": "text/html"});
 
-<style>
-#meniulateral {
-  background: #e6e6e6;
-  height: 100%;
-  width: 15%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  /*display: none;*/
-}
-
-#meniusus {
-  background: #282c34;
-  height: 5%;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0
-}
-
-#restulpaginii {
-  background: #3b5596;
-  height: 95%;
-  width: 85%;
-  position: fixed;
-  top: 5%;
-  left: 15%;
-}
-
-#pagina123 {
-  position: absolute;
-  top: 100px;
-  left: 100px;
-}
-
-</style>
-		`);
-
+	var text = "salut";
+	fs.readFile("./site/index.html", "UTF-8", function(err, output){
+		if(err) throw err;
+		res.end(output);
+	})
 });
-
 server.listen(3000);
-
-console.log("Server listening on port 3000");
+console.log("Server is listening on port 3000");
