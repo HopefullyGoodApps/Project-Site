07f603b1b8935896f602657141e77914af81d79d
SuciuAlin <suciualinflorin@gmail.com>
2017-12-14 20:39:56 +0200

    Meniu lateral trigger
    Meniul lateral se va afisa de fiecare data cand se apasa undeva pe pagina, iar la o noua apasare va disparea. Inca functioneaza si butoanele ce servesc in acest scop.

--- Server/Server.js
+++ Server/Server.js
@@ -5,7 +5,7 @@ var server = http.createServer(function(req,res){
 	res.writeHead(200, {"Content-Type": "text/html"});
 
 	var text = "salut";
-	fs.readFile("./site/index.html", "UTF-8", function(err, output){
+	fs.readFile("../site/index.html", "UTF-8", function(err, output){
 		if(err) throw err;
 		res.end(output);
 	})
